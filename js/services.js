44ce40d9dd51c68540939dfe77d91e7fe88efe07
Ayush Anil Sankhe <124515972+Ayushsankhe@users.noreply.github.com>
2025-01-19 12:08:25 +0530

    kkk

---
+++ js/services.js
@@ -0,0 +1,8 @@
+function toggleServices() {
+    var extraServices = document.getElementById('extra-services');
+    if (extraServices.style.display === "none" || extraServices.style.display === "") {
+        extraServices.style.display = "grid"; // Show the extra services
+    } else {
+        extraServices.style.display = "none"; // Hide the extra services again
+    }
+}
